b54856e931bc2da632b0b1a9174e8d41e4b4dcb9
DavidBonvin <ledave.musique@gmail.com>
2023-02-15 17:03:40 +0100

    the user can register and log in to the app, the app is secure on both the frontend and the backend.

--- src/app/components/Login/LoginCard.jsx
+++ src/app/components/Login/LoginCard.jsx
@@ -4,6 +4,7 @@ import React, { useEffect, useRef, useState } from "react";
 import Button from "../../components/base/Button";
 import Input from "../../components/base/Input";
 import ReCAPTCHA from "react-google-recaptcha";
+import axios from "axios";
 import { useFormik } from "formik";
 import validationSchema from "../../utils/loginSchema";
 
@@ -16,14 +17,15 @@ Les variables des couleurs se trouvent dans le fichier tailwind.config.js
 
 function LoginCard() {
   const navigate = useNavigate();
-
-  const [captchaValidate, setcaptchaValidate] = useState(null);
-  const [formValidate, setFormValidate] = useState(null);
-  const [isLogin, setIsLogin] = useState(false);
+  
+  const [captchaValidate, setcaptchaValidate] = useState(null)
+  const [formValidate, setFormValidate] = useState(null)
+  const [isLogin, setIsLogin] = useState(false)
+  const [errorLog, setErrorLog] = useState(false)
 
   useEffect(() => {
-    isLogin && navigate("/register");
-  }, [isLogin]);
+    isLogin && navigate('/')
+  }, [isLogin])
 
   const captchagoogle = useRef(null);
 
@@ -42,100 +44,94 @@ function LoginCard() {
     password: "",
   };
 
-  const {
-    values,
-    handleChange,
-    handleBlur,
-    isSubmitting,
-    isValid,
-    touched,
-    handleSubmit,
-    setFieldError,
-    resetForm,
-    errors,
-  } = useFormik({
+  const { 
+    values, handleChange, handleBlur,
+    isSubmitting, isValid, touched, 
+    handleSubmit, setFieldError,
+    resetForm, errors } = useFormik({
     initialValues,
     validationSchema,
-    onSubmit,
+    onSubmit
   });
 
+  const login = (values) => {
+    const user = values
+    axios
+      .post("http://localhost:8000/tree-up-api/login", user)
+      .then(response => {
+        console.log('requête login ok')
+        setIsLogin(true)
+      })
+      .catch(error => {
+        console.log('erreur requête login')
+        setErrorLog(true)
+      }) 
+    console.log('requête terminée')
+  }
+  
   async function onSubmit(formValues) {
     console.log(formValues);
-    if (captchagoogle.current.getValue()) {
-      setFormValidate(true);
-      setcaptchaValidate(true);
-      try {
-        await register(formValues);
-        resetForm();
-        console.log("Connexion réussie");
-        setIsLogin(true);
-      } catch ({ errors }) {
-        for (let key in errors) {
-          setFieldError(key, errors[key]);
-        }
-      }
+    if(captchagoogle.current.getValue()) {
+      setFormValidate(true)
+      setcaptchaValidate(true)
+      login(formValues);
+      resetForm();
+      console.log('Connexion réussie')
+      // setIsLogin(true)
     } else {
-      setFormValidate(false);
-      setcaptchaValidate(false);
+      setFormValidate(false)
+      setcaptchaValidate(false)
     }
   }
 
+
   return (
-    <div className="bg-gray-1 shadow-lg w-full max-w-2xl md:w-4/5 lg:w-4/5 2xl:max-w-3xl rounded-lg flex flex-col items-center">
-      <form
-        onSubmit={handleSubmit}
-        className="p-2 bg-transparent w-full sm:w-4/5 md:w-4/5 lg:w-4/5 2xl:w-4/5 m-2 rounded-lg flex flex-col justify-center items-center"
-      >
-        <h2 className="my-8 text-2xl sm:text-3xl lg:text-4xl">S'identifier</h2>
+    <div className='bg-gray-1 w-full max-w-2xl md:w-4/5 lg:w-4/5 2xl:max-w-3xl rounded-lg flex flex-col  items-center'>
+      <form onSubmit={handleSubmit} className='p-2 bg-transparent w-full sm:w-4/5 md:w-4/5 lg:w-4/5 2xl:w-4/5 m-2 rounded-lg flex flex-col justify-center items-center'>
+        <h2 className='my-8 text-2xl sm:text-3xl lg:text-4xl'>S'identifier</h2>
         <Input
-          type="email"
-          placeholder="exemple@gmail.com"
+          type='email'
+          placeholder='exemple@gmail.com'
           description="Adresse mail enregistrée lors de l'inscription"
           name="email"
           value={values.email}
           onChange={handleChange}
           onBlur={handleBlur}
         />
-        {touched.email && errors.email && (
-          <small className="error">{errors.email}</small>
-        )}
+        { touched.email && errors.email && 
+          <small className="error">{ errors.email }</small>
+        }
         <Input
-          type="password"
-          placeholder="********"
+          type='password'
+          placeholder='********'
           description="Vous avez oublié votre mot de passe"
           name="password"
           value={values.password}
           onChange={handleChange}
           onBlur={handleBlur}
         />
-        {touched.password && errors.password && (
-          <small className="error">{errors.password}</small>
-        )}
-        <ReCAPTCHA
-          className="my-4"
-          ref={captchagoogle}
-          sitekey="6Lc43mskAAAAAPGuj5wsQMpI-Bkcvy1cpXJusonn"
-          onChange={onChange}
-        />
-        {captchaValidate === false && (
-          <p className="w-full text-red-600 text-center mb-4">
-            Etes-vous un robot ? Merci de valider le captcha
-          </p>
-        )}
+        { touched.password && errors.password && 
+          <small className="error">{ errors.password }</small>
+        }
+        <ReCAPTCHA className="my-4" ref={captchagoogle} sitekey="6Lc43mskAAAAAPGuj5wsQMpI-Bkcvy1cpXJusonn" onChange={onChange} />
+        {captchaValidate === false &&
+          <p className="w-full text-red-600 text-center mb-4">Etes-vous un robot ? Merci de valider le captcha</p>
+        }
         <Button
           className="w-full sm:w-4/5"
           title="CONNEXION"
           type="submit"
           disabled={isSubmitting || !isValid}
         />
+        { errorLog && 
+          <small className="error text-center mt-4">Paire login / mot de passe incorrecte</small>
+        }
       </form>
-      <div className="mb-4 w-full flex justify-end">
-        <Link className="mr-4 mt-4" to="/signin">
-          Créer un compte
-        </Link>
+      <div className='mb-4 w-full flex justify-end'>
+        <Link className='mr-4 mt-4' to='/register'>Créer un compte</Link>
       </div>
     </div>
-  );
+  )
 }
 
-export default LoginCard;
+export default LoginCard
