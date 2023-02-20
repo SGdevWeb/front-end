d6cd686481821a1a74faf44eeb7603776e636d34
AlexandreFoucherDEV <foucher.alex@gmail.com>
2023-02-20 13:37:10 +0100

    dossier editprofile

---
+++ src/app/components/EditProfile/ProfileBoxEdit.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const ProfileBoxEdit = ({ name, date, location, description }) => (
+  <div className="bg-white rounded-lg mx-5 p-5">
+    <div className="flex justify-between">
+      <p className="text-xl font-bold">{name}</p>
+      {date && <p className="text-xs">{date}</p>}
+    </div>
+    {location && <p className="text-xs">{location}</p>}
+    <hr className="my-2" />
+    <p className="font-bold">Description</p>
+    <p className="text-sm">{description}</p>
+  </div>
+);
+
+export default ProfileBoxEdit;
