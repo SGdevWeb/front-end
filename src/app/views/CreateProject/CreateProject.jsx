ad60701d97dde460d6968107edc6688bcf0e4ffd
A2x-l <jt.allard1@gmail.com>
2023-03-07 14:05:22 +0100

    Revert "Merge branch 'UpdateProjectBis' into ViewProject"
    This reverts commit 35299150479e1a42cf2211f70048ab3b6345da05, reversing
    changes made to 247d49e91a272ac055294cf2e316c0af8873fc2e.

--- src/app/views/CreateProject/CreateProject.jsx
+++ src/app/views/CreateProject/CreateProject.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
 
 import Button from "../../components/Base/ButtonBis";
 import InputBis from "../../components/base/InputBis";
@@ -8,155 +7,65 @@ import TextArea from "../../components/base/TextArea";
 import apiGateway from "../../api/backend/apiGateway";
 import typesProject from "../../fakeData/TypeData";
 import { useFormik } from "formik";
+import { useNavigate } from "react-router-dom";
 import validationSchema from "../../utils/createProjectSchema";
 
-export default function CreateProject({isEditMode}) {
+export default function CreateProject() {
+
+	const navigate = useNavigate();
+
+	const [error, setError] = useState();
 	
-  const navigate = useNavigate();
+	const initialValues = {
+		name: "",
+		date_start: "",
+		date_end: "",
+		type: "",
+		description: "",
+	};
 
-  const [error, setError] = useState();
-  
-  const {uuid} = useParams(); 
-  
+	const onSubmit = async (formValues) => {
+		try {
+			if (formValues.date_end === "") delete formValues.date_end;
+			const response = await apiGateway.post("/project/create/", formValues);
+			resetForm();
+			navigate("/project/" + response.data.uuid);
+		} catch (error) {
+			setError(error.message);
+		}
+	};
 
-  const fakeData = {
-    name: "EspaceDeVie",
-    date_start: "2023-03-02T00:00:00.000Z",
-    date_end: "",
-    type: "",
-    description: "Manu",
-  }
-  
-  const project = fakeData;
-  
-  const initialValues = isEditMode
-    ? {
-        name: project.name,
-        date_start: project.date_start.slice(0,project.date_start.indexOf("T")),
-        date_end: project.date_end.slice(0,project.date_start.indexOf("T")),
-        type: project.type,
-        description: project.description,
-      } : { 
-			name: "",
-			date_start: "",
-			date_end: "",
-			type: "",
-			description: "",
-	  }
+	const { handleSubmit, values, touched, isValid, isSubmitting, handleChange, handleBlur, resetForm, errors } = useFormik({
+		initialValues,
+		validationSchema,
+		onSubmit,
+	});
 
-  const onSubmit = async (formValues) => {
-    try {
-      if (formValues.date_end === "") delete formValues.date_end;
-      delete formValues.type;
-      const response = isEditMode ? await apiGateway.put(`/project/update/${uuid}`, formValues) : await apiGateway.post("/project/create/", formValues);
-      resetForm();
-      navigate("/project/" + response.data.uuid);
-    } catch (error) {
-      setError(error.message);
-    }
-  };
+	return (
+		<form className="p-3 bg-gray-1" onSubmit={handleSubmit}>
+			<img className="rounded-xl justify-center items-center" src={"https://dummyimage.com/1200x300.png/5fa2dd/ffffff"} alt="Projet" loading="lazy" />
+			<Button className="mt-3" title="Ajouter une image" />
+			<div className="flex justify-between pt-3">
+				<div className="flex flex-col gap-1">
+					<InputBis type="text" placeholder="Nom du projet" name="name" value={values.name} onChange={handleChange} onBlur={handleBlur} />
+					{touched.name && errors.name && <small className="error">{errors.name}</small>}
 
-  const {
-    handleSubmit,
-    values,
-    touched,
-    isValid,
-    isSubmitting,
-    handleChange,
-    handleBlur,
-    resetForm,
-    errors,
-  } = useFormik({
-    initialValues,
-    validationSchema,
-    onSubmit,
-  });
+					<Select label={"Type de projet"} dataList={typesProject} name="type" value={values.type} onChange={handleChange} onBlur={handleBlur} />
+					{touched.type && errors.type && <small className="error">{errors.type}</small>}
+				</div>
+				<div className="flex flex-col gap-1">
+					<InputBis type="date" placeholder="Date de Début" name="date_start" value={values.date_start} onChange={handleChange} onBlur={handleBlur} />
+					{touched.date_start && errors.date_start && <small className="error">{errors.date_start}</small>}
 
-  return (
-    <form className="p-3 bg-gray-1" onSubmit={handleSubmit}>
-      <img
-        className="rounded-xl justify-center items-center"
-        src={"https://dummyimage.com/1200x300.png/5fa2dd/ffffff"}
-        alt="Projet"
-        loading="lazy"
-      />
-      <Button className="mt-3" title="Ajouter une image" />
-      <div className="flex justify-between pt-3">
-        <div className="flex flex-col gap-1">
-          <InputBis
-            type="text"
-            placeholder="Nom du projet"
-            name="name"
-            value={values.name}
-            onChange={handleChange}
-            onBlur={handleBlur}
-          />
-          {touched.name && errors.name && (
-            <small className="error">{errors.name}</small>
-          )}
-
-          <Select
-            label={"Type de projet"}
-            dataList={typesProject}
-            name="type"
-            value={values.type}
-            onChange={handleChange}
-            onBlur={handleBlur}
-          />
-          {touched.type && errors.type && (
-            <small className="error">{errors.type}</small>
-          )}
-        </div>
-        <div className="flex flex-col gap-1">
-          <InputBis
-            type="date"
-            placeholder="Date de Début"
-            name="date_start"
-            value={values.date_start}
-            onChange={handleChange}
-            onBlur={handleBlur}
-          />
-          {touched.date_start && errors.date_start && (
-            <small className="error">{errors.date_start}</small>
-          )}
-
-          <InputBis
-            type="date"
-            placeholder="Date de Fin"
-            name="date_end"
-            value={values.date_end}
-            onChange={handleChange}
-            onBlur={handleBlur}
-          />
-          {touched.date_end && errors.date_end && (
-            <small className="error">{errors.date_end}</small>
-          )}
-        </div>
-      </div>
-      <Button className="my-3" title="Ajouter des collaborateurs" />
-      <TextArea
-        placeholder={"Description du projet"}
-        className="w-full"
-        rows={"10"}
-        name="description"
-        value={values.description}
-        onChange={handleChange}
-        onBlur={handleBlur}
-      />
-      {touched.description && errors.description && (
-        <small className="error">{errors.description}</small>
-      )}
-      {error && (
-        <p className="error p-5 m-1 border-2 border-red-700 bg-white">
-          {error}
-        </p>
-      )}
-      <Button
-        type="submit"
-        disabled={!isValid || isSubmitting}
-        className="flex mx-auto mt-3"
-        title="Créer le projet"
-      />
-    </form>
-  );
+					<InputBis type="date" placeholder="Date de Fin" name="date_end" value={values.date_end} onChange={handleChange} onBlur={handleBlur} />
+					{touched.date_end && errors.date_end && <small className="error">{errors.date_end}</small>}
+				</div>
+			</div>
+			<Button className="my-3" title="Ajouter des collaborateurs" />
+			<TextArea placeholder={"Description du projet"} className="w-full" rows={"10"} name="description" value={values.description} onChange={handleChange} onBlur={handleBlur} />
+			{touched.description && errors.description && <small className="error">{errors.description}</small>}
+			{error && <p className="error p-5 m-1 border-2 border-red-700 bg-white">{error}</p>}
+			<Button type="submit" disabled={!isValid || isSubmitting} className="flex mx-auto mt-3" title="Créer le projet" />
+		</form>
+	);
 }
--- src/app/views/Home/Home.jsx
+++ src/app/views/Home/Home.jsx
@@ -1,5 +1,3 @@
-import Button from "../../components/Base/ButtonBis";
-import { Link } from "react-router-dom";
 import ListProject from "../../components/Project/ListProject";
 import React from "react";
 
@@ -7,7 +5,6 @@ const Home = () => {
 
   return (
     <div className="h-full">
-      <Link to={"project/update/5078bfc7-65ed-4472-8a3e-9f2aabfb30e7"}><Button title="salut"></Button></Link>
       <ListProject />
     </div>
   );
