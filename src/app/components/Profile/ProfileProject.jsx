ed00e57622dd16c9bc0bfed5b57c60c5fa90c6a8
Emmanuel <caronnunu@gmail.com>
2023-03-09 14:22:33 +0100

    listing project in home page and profil

--- src/app/components/Profile/ProfileProject.jsx
+++ src/app/components/Profile/ProfileProject.jsx
@@ -1,13 +1,24 @@
 import "../../views/Profile/Scrollbar.css";
 
+import React, { useEffect, useState } from "react";
 import CardProject from "../Project/CardProject";
 import { Link } from "react-router-dom";
 import { PlusIcon } from "@heroicons/react/solid";
-import React from "react";
+import { URL_BACK_PROJECT } from "../../constants/urls/urlBackEnd";
 import { URL_CREATEPROJECT } from "../../constants/urls/urlFrontEnd";
-import projectData from "../../fakeData/ProjectData";
+import apiGateway from "../../api/backend/apiGateway";
 
-const ProfileProject = () => {
+const ProfileProject = ({uuid = "744966ab-07f3-48b3-9799-4b074c553230"}) => {
+
+  const [projects, setProjects] = useState([]);
+	const [error, setError] = useState();
+
+  useEffect(() => {
+		apiGateway.get(URL_BACK_PROJECT + "/user/" + uuid)
+		.then((({data}) => setProjects(data)))
+		.catch(({error}) => setError(error.message));
+	}, []);
+  
   return (
     <div className="flex ml-7">
       <div className="">
@@ -16,9 +27,14 @@ const ProfileProject = () => {
         </Link>
       </div>
       <div className="flex gap-3 overflow-x-scroll w-auto border-red-500 scrollbar">
-      {projectData.map((project) => (
+      {projects.map((project) => (
         <CardProject key={project.uuid} {...project} />
       ))}
+      {error && (
+				<p className="error p-5 m-1 border-2 border-red-700 bg-white">
+				{error}
+				</p>
+			)}
       </div>
     </div>
   );
--- src/app/components/Project/ListProject.jsx
+++ src/app/components/Project/ListProject.jsx
@@ -1,14 +1,25 @@
+import React, { useEffect, useState } from "react";
+
 import CardProject from "./CardProject";
 import { Link } from "react-router-dom";
-import React from "react";
+import { URL_BACK_PROJECT } from "../../constants/urls/urlBackEnd";
 import { URL_CREATEPROJECT } from "../../constants/urls/urlFrontEnd";
-import projectData from "../../fakeData/ProjectData.js"
+import apiGateway from "../../api/backend/apiGateway"
 
 const ListProject = () => {
 
+	const [projects, setProjects] = useState([]);
+	const [error, setError] = useState();
+	
+	useEffect(() => {
+		apiGateway.get(URL_BACK_PROJECT)
+		.then((({data}) => setProjects(data)))
+		.catch(({error}) => setError(error.message));
+	}, []);
+
 	return (
-		<div className="flex flex-wrap justify-center h-full gap-10">
-			<div className="border-gradient-v rounded-3xl p-3">
+		<div className="flex flex-wrap justify-center content-start h-full gap-10">
+			<div className="border-gradient-v rounded-3xl p-3 h-fit">
 				<p>Tree-Up vous permets de partager et d’échanger sur les divers projets d’actualité postés par nos jeunes développeurs !</p>
 				<p>N’hésitez pas à faire un tour sur nos projets récents et de partager votre avis dans la section commentaires !</p>
 				<Link to={URL_CREATEPROJECT} className="text-primary-light">
@@ -16,9 +27,16 @@ const ListProject = () => {
 				</Link>
 				<p className="inline-block ml-1">si vous souhaitez créer un projet afin de le mettre en avant !</p>
 			</div>
-			{projectData.map((project) => (
+			{projects
+				.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
+				.map((project) => (
 				<CardProject key={project.uuid} {...project} />
 			))}
+			{error && (
+				<p className="error p-5 m-1 border-2 border-red-700 bg-white">
+				{error}
+				</p>
+			)}
 		</div>
 	);
 };
