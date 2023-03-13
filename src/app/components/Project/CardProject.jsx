b27ea8110fb9666f8e1047d60fa9218222974622
Emmanuel <caronnunu@gmail.com>
2023-03-13 12:28:58 +0100

    Fix description (end with "...") in card project

--- src/app/components/Project/CardProject.jsx
+++ src/app/components/Project/CardProject.jsx
@@ -7,27 +7,14 @@ const CardProject = ({ uuid, name, type, description, collaborator, technologies
 	return (
 		<Link to={URL_PROJETVIEW + uuid} className="border-gradient-h border-0 rounded-2xl hover:shadow-2xl active:border-2">
 			<article className="flex flex-col items-center pb-2 w-96 rounded-2xl bg-white">
-				{/* <img className="rounded-t-2xl h-40 w-full aspect-video" src={image} alt="Projet" loading="lazy" /> */}
-
 				<div className="flex flex-col w-full  justify-between align-super p-3">
 					<div>
 						<h4 className="text-2xl">{name}</h4>
-						{/* <h6 className="text-sm text-gray-400">Type de project : {type}</h6> */}
 					</div>
 					<div>
 						<h5 className="text-sm font-bold">Description</h5>
-						<p className="break-words">{description.slice(0, 150)}...</p>
-					</div>{/*
-					<hr className="border-dark" />
-					 <div>
-						<h5 className="text-sm font-bold">Collarateurs</h5>
-						<p>{collaborator}</p>
-					</div> 
-					<div className="flex gap-1 overflow-x-auto">
-						 {technologies.map((technologie) => (
-							<PillTechnologie key={technologie.uuid} {...technologie} />
-						))} 
-					</div>*/}
+						<p className="break-words">{description.slice(0, 150) + (description.length > 149 ? "..." : "")}</p>
+					</div>
 				</div>
 			</article>
 		</Link>
