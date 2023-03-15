8ad19d4eb56f9e83a3eb04099cfd68fe0b2ec4e7
Emmanuel <caronnunu@gmail.com>
2023-03-15 14:47:34 +0100

    add min 100 caracteres in project description

--- src/app/utils/createProjectSchema.js
+++ src/app/utils/createProjectSchema.js
@@ -6,5 +6,8 @@ export default Yup.object({
     name: Yup.string().required('Le champ name est requis.').matches(PROJECT_NAME, 'Veuillez saisir un nom de projet valide.'),
     date_start: Yup.date().required('Le champ date de debut est requis.'),
     date_end: Yup.date(),
-    description: Yup.string().required('Le champ description est requis.').max(5000, 'La description ne doit pas dépasser 5000 caractères.'),
+    description: Yup.string()
+        .required('Le champ description est requis.')
+        .min(100, "La description doit contenir au moins 100 caractères.")
+        .max(5000, 'La description ne doit pas dépasser 5000 caractères.'),
 });
