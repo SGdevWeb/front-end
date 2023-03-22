7ed7205a0932bd4a63a5d64a78e5198f5f390301
Emmanuel <caronnunu@gmail.com>
2023-03-22 10:16:18 +0100

    [creation projet][FIX][EC] - remove useless space in form value on submit

--- src/app/utils/createProjectSchema.js
+++ src/app/utils/createProjectSchema.js
@@ -3,10 +3,10 @@ import * as Yup from 'yup';
 import { PROJECT_NAME } from '../constants/regex';
 
 export default Yup.object({
-    name: Yup.string().required('Le champ name est requis.').matches(PROJECT_NAME, 'Veuillez saisir un nom de projet valide.'),
+    name: Yup.string().trim().required('Le champ name est requis.').matches(PROJECT_NAME, 'Veuillez saisir un nom de projet valide.'),
     date_start: Yup.date().required('Le champ date de debut est requis.'),
     date_end: Yup.date(),
-    description: Yup.string()
+    description: Yup.string().trim()
         .required('Le champ description est requis.')
         .min(100, "La description doit contenir au moins 100 caractères.")
         .max(5000, 'La description ne doit pas dépasser 5000 caractères.'),
