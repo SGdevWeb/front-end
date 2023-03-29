0b7e76a61c3013c13c6944e6951b641c0eddb04f
A2x-l <jt.allard1@gmail.com>
2023-03-29 14:17:01 +0200

    Fix EmptyComment

--- src/app/components/Project/NewComment.jsx
+++ src/app/components/Project/NewComment.jsx
@@ -4,20 +4,16 @@ import { commentPost } from "../../api/backend/comment";
 
 function NewComment({ addComment, uuid_project }) {
   const [inputValue, setInputValue] = useState("");
-  const [error, setError] = useState(null);
+  const [error, setError] = useState(false);
   const [charCount, setCharCount] = useState(0);
   const [isTextareaFocused, setIsTextareaFocused] = useState(false);
   const textareaRef = useRef(null);
 
   useEffect(() => {
-    if (inputValue.length === 0 && error !== null) {
+    if (inputValue.trim().length === 0 && isTextareaFocused) {
       setError(true);
-    }
-    if (inputValue.length > 0) {
+    } else {
       setError(false);
-    }
-    if (!isTextareaFocused) {
-      setError(null);
     }
     setCharCount(inputValue.length);
   }, [inputValue, isTextareaFocused]);
@@ -25,9 +21,10 @@ function NewComment({ addComment, uuid_project }) {
   function handleSubmit(e) {
     e.preventDefault();
     textareaRef.current.focus();
-    const value = inputValue;
+    const value = inputValue.trim();
     if (!value) {
-      return setError(true);
+      setError(true);
+      return;
     }
     const newComment = {
       comment: value,
@@ -35,9 +32,10 @@ function NewComment({ addComment, uuid_project }) {
     };
     commentPost(newComment)
       .then(() => {
-        setError(null);
+        setError(false);
         setInputValue("");
         addComment();
+        textareaRef.current.blur();
       })
       .catch((error) => console.log(error));
   }
@@ -68,7 +66,7 @@ function NewComment({ addComment, uuid_project }) {
           type="submit"
           onClick={handleSubmit}
         >
-          <span className="text-3xl font-bold text- text-transparent bg-clip-text gradient mr-1">
+          <span className="3xl font-bold text- text-transparent bg-clip-text gradient mr-1">
             {">"}
           </span>
         </button>
