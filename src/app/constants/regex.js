46b18b2994e864c0e2b1c7c7fbf5ddda961a2e79
A2x-l <jt.allard1@gmail.com>
2023-03-30 10:30:15 +0200

    Edit regEx firstname and lastname

--- src/app/constants/regex.js
+++ src/app/constants/regex.js
@@ -1,9 +1,9 @@
 export const PASSWORD_VALID =
   /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
 
-export const FIRSTNAME = /^[a-zA-ZÀ-ÿ\-]+$/;
+export const FIRSTNAME = /^[a-zA-Zàáâäãåçèéêëìíîïñòóôöõøùúûüýÿ'\-]{2,}$/;
 
-export const LASTNAME = /^[a-zA-ZÀ-ÿ\s\-]+$/;
+export const LASTNAME = /^[a-zA-Zàáâäãåçèéêëìíîïñòóôöõøùúûüýÿ'\-\s]{2,}$/;
 
 export const PROJECT_NAME = /^.{3,50}$/;
 
