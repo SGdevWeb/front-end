baf62756148f6a62643c544af663ced58258d691
Emmanuel <caronnunu@gmail.com>
2023-04-24 11:59:17 +0200

    [Refont like][REFACTO][EC] : refont button like on project

--- src/app/components/Project/LikeButton.jsx
+++ src/app/components/Project/LikeButton.jsx
@@ -1,42 +1,47 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 
-import { ThumbUpIcon as HeartIconEmpty } from "@heroicons/react/outline";
 import { ThumbUpIcon as HeartIconfull } from "@heroicons/react/solid";
-import {postLike} from '../../api/backend/like'
+import { postLike } from "../../api/backend/like";
 
-function LikeButton({ isLogged, project, setProject}) {
+function LikeButton({ isLogged, project, setProject }) {
+	const like = async () => {
+		if (!isLogged) return;
+
+		await postLike({ uuid_project: project.uuid })
+			.then((res) => {
+				const newProject = project;
+				{
+					project.liked
+						? (newProject.liked = false)
+						: (newProject.liked = true);
+				}
+				{
+					res.data.isliked
+						? (newProject.countLikes += 1)
+						: (newProject.countLikes -= 1);
+				}
+				setProject((project) => ({ ...project, ...newProject }));
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+		//setlikes(likes+1)
+	};
+
 	return (
-        <div className={`bg-gray-200 text-center text-sm hover:font-medium flex items-center gap-2 py-1 px-3 rounded-l-md cursor-pointer ${project.liked ? "bg-zinc-800 text-white" : ""}`}>
-            
-            {isLogged ? (
-                <button 
-                    type="button" 
-                    className={`mr-1`} 
-                    onClick={async () => {
-                        await postLike({uuid_project : project.uuid}).then((res) => {
-                            
-                            const newProject = project
-                            {project.liked ? newProject.liked = false : newProject.liked = true}
-                            {res.data.isliked ? newProject.countLikes+=1 : newProject.countLikes-=1}
-                            setProject(project => ({...project,...newProject}));
-                        }).catch((err) => {
-                            console.log(err)
-                        })
-                        //setlikes(likes+1)
-                    }}
-                > 
-                {project.liked ? (
-                <HeartIconfull className='h-7 w-7 my-1'/>
-                ) : (
-                <HeartIconEmpty className='h-7 w-7 my-1'/>
-                )}
-			    
-		        </button>
-            ) : <HeartIconEmpty className='h-7 w-7 my-1 mr-1'/>
-            }
-            {project.liked ? "Vous avez like ce projet" : "Ajouter un like"}
-        </div>
-		
+		<div
+			className={`bg-gray-200 text-center text-sm hover:font-medium py-1 px-3 rounded-l-md cursor-pointer ${
+				project.liked && "bg-zinc-800 text-white"
+			}`}
+		>
+			<button type="button" onClick={like} className="flex items-center gap-2">
+				<HeartIconfull
+					className="h-6 w-6 my-1"
+					color={project.liked && "white"}
+				/>
+				{project.liked ? "Vous avez like ce projet" : "Ajouter un like"}
+			</button>
+		</div>
 	);
 }
 
